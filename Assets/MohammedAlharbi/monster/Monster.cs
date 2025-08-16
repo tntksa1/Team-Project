178bf59c82424ef264a3dac338e5ab160be38463
tntksa1 <tntksa5@gmail.com>
2025-08-16 17:57:52 +0300

    Add Monster

---
+++ Assets/MohammedAlharbi/monster/Monster.cs
@@ -0,0 +1,35 @@
+using TreeEditor;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    public Transform a;
+    public Transform b;
+    public float speed;
+    public Animator ainm;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        transform.LookAt(a);
+        ainm = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("a"))
+        {
+            transform.LookAt(b);
+        }
+        else if (other.gameObject.CompareTag("b"))
+        {
+            transform.LookAt(a);
+        }
+    }
+}
